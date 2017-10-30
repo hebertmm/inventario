003e540dc636e0f132e963e63c77a8e631c418d6
hebert <hebertmm@gmail.com>
2017-10-30 15:16:28 -0200

    Implementação da UI de configurações.

---
+++ app/src/main/java/com/example/hebert/inventario/activity/FileGeneratorActivity.java
@@ -0,0 +1,15 @@
+package com.example.hebert.inventario.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.hebert.inventario.R;
+
+public class FileGeneratorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_file_generator);
+    }
+}
